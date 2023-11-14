0ccd9e158c19cb639f1d54ddee15c9cd1fa12d96
VW <fds.vctr@gmail.com>
2023-11-14 21:14:40 +0300

    fresh

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { DocumentBuilder } from "@nestjs/swagger";
 
 
 async function start() {
     const PORT = process.env.PORT || 3000;
     const app = await NestFactory.create(AppModule)
 
+    const confg = new DocumentBuilder()
+    .setTitle('My First backend work')
+    .setDescription('Doc for REST API')
+    .setVersion('0.0.1')
+    .addTag('VW')
+    .build()
     await app.listen(PORT, ()=> console.log(`server was started at http://localhost:${PORT}`))
 }
 start()
